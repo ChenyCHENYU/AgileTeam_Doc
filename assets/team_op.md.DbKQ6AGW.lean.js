c62645dceb2b940e39b4dcdee698f216214b417a
ChenYu <ycyplus@gmail.com>
2025-07-15 00:15:31 +0800

    Updates

--- assets/team_op.md.DbKQ6AGW.lean.js
+++
@@ -1,8 +0,0 @@
-/**
- * name: agile-team-doc
- * version: v2.1.0
- * description: 敏捷团队构建的线上文档，希望同时帮助到团队伙伴和社区的大家
- * author: CHENY <ycyplus@gmail.com>
- * homepage: https://github.com/ChenyCHENYU/AgileTeam_Doc
- */
-import{V as l,a as n,c as d}from"./chunks/theme.ComXixBD.js";import{c as i,o as p,G as a,w as t,k as s,a as r}from"./chunks/framework.BjhQmtN6.js";import{b as m}from"./chunks/baseURL.D0AlTPBa.js";const b=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"page"},"headers":[],"relativePath":"team/op.md","filePath":"team/op.md","lastUpdated":1732265191000}'),c={name:"team/op.md"},V=Object.assign(c,{setup(u){const o=[{avatar:`${m}/薛旭杰.png`,name:"薛旭杰",title:"DevOps Lead",desc:"洞察力是取胜的关键，一定不要让你的警觉松懈!"},{avatar:`${m}/郭森淼.png`,name:"郭森淼",title:"Big Data",desc:"Dare and the world always yields"}];return(f,e)=>(p(),i("div",null,[a(s(d),null,{default:t(()=>[a(s(l),null,{title:t(()=>e[0]||(e[0]=[r("我们的运维")])),lead:t(()=>e[1]||(e[1]=[r("核心成员")])),_:1}),a(s(n),{size:"medium",members:o})]),_:1})]))}});export{b as __pageData,V as default};
--- assets/team_ui.md.DCAqBlDa.lean.js
+++
@@ -1,8 +0,0 @@
-/**
- * name: agile-team-doc
- * version: v2.1.0
- * description: 敏捷团队构建的线上文档，希望同时帮助到团队伙伴和社区的大家
- * author: CHENY <ycyplus@gmail.com>
- * homepage: https://github.com/ChenyCHENYU/AgileTeam_Doc
- */
-import{V as o,a as n,b as d,c as p}from"./chunks/theme.ComXixBD.js";import{c as u,o as g,G as t,w as a,k as s,a as m}from"./chunks/framework.BjhQmtN6.js";import{b as r}from"./chunks/baseURL.D0AlTPBa.js";const V=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"page"},"headers":[],"relativePath":"team/ui.md","filePath":"team/ui.md","lastUpdated":1732265191000}'),c={name:"team/ui.md"},_=Object.assign(c,{setup(f){const i=[{avatar:`${r}/刘凯.png`,name:"刘凯",title:"Ui Design Lead",desc:"有道无术术可求，有术无道止于术"}],l=[{avatar:`${r}/傅会文.png`,name:"傅会文",title:"Ui Design",desc:"UI出品，必是精品"},{avatar:`${r}/廉国崴.png`,name:"廉国崴",title:"Ui Design",desc:"凡事预则立，不预则废"},{avatar:`${r}/刘旭洋.png`,name:"刘旭洋",title:"Modeler Tech Lead",desc:"成大事者，争百年，不争一息"},{avatar:`${r}/许扬沛.png`,name:"许扬沛",title:"Modeler",desc:"耐心和持久胜过激烈和狂热"}];return(b,e)=>(g(),u("div",null,[t(s(p),null,{default:a(()=>[t(s(o),null,{title:a(()=>e[0]||(e[0]=[m("我们的设计")])),lead:a(()=>e[1]||(e[1]=[m("核心成员")])),_:1}),t(s(n),{size:"medium",members:i}),t(s(d),null,{title:a(()=>e[2]||(e[2]=[m("特别感谢")])),lead:a(()=>e[3]||(e[3]=[m("协同伙伴")])),members:a(()=>[t(s(n),{size:"small",members:l})]),_:1})]),_:1})]))}});export{V as __pageData,_ as default};
